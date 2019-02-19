ad15a09a23f9021227e0a59f570ebf765834ec3c
Muhammad Ismail <47635180+ismardoi2001@users.noreply.github.com>
2019-02-19 19:38:49 +0500

    update

--- javascript.js
+++ javascript.js
@@ -4,7 +4,7 @@ const bodyBcg = document.querySelector('body');
 const colors=['yellow','red','green','blue'];
 
 colorBtn.addEventListener('click',changeColor);
-
+//fuction 
 function changeColor(){
   //  bodyBcg.style.backgroundColor =colors[2];
   let random =Math.floor(Math.random()*colors.length)
